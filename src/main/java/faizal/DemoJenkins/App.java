bcd2e9866778d041cae06f6d560f4d193d7a0d9e
shada <shadabfaizal7@gmail.com>
2020-12-05 14:54:44 +0530

    initial commit

---
+++ src/main/java/faizal/DemoJenkins/App.java
@@ -0,0 +1,13 @@
+package faizal.DemoJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
